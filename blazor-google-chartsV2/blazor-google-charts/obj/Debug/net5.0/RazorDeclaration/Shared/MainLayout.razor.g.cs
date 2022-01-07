12f5bd4dd7c09f1ebc26baa4f9f79f241fac4f29
Jakub Drahotský <kubadrahotsky@gmail.com>
2022-01-07 13:45:36 +0100

    before reverse

--- blazor-google-chartsV2/blazor-google-charts/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ blazor-google-chartsV2/blazor-google-charts/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,77 +13,77 @@ namespace BlazorGoogleCharts.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "F:\C_sharp\2021_12_27\EDS_calculator\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
+#line 1 "C:\Projects\Programs\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "F:\C_sharp\2021_12_27\EDS_calculator\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
+#line 2 "C:\Projects\Programs\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "F:\C_sharp\2021_12_27\EDS_calculator\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
+#line 3 "C:\Projects\Programs\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "F:\C_sharp\2021_12_27\EDS_calculator\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
+#line 4 "C:\Projects\Programs\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "F:\C_sharp\2021_12_27\EDS_calculator\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
+#line 5 "C:\Projects\Programs\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "F:\C_sharp\2021_12_27\EDS_calculator\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
+#line 6 "C:\Projects\Programs\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "F:\C_sharp\2021_12_27\EDS_calculator\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
+#line 7 "C:\Projects\Programs\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "F:\C_sharp\2021_12_27\EDS_calculator\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
+#line 8 "C:\Projects\Programs\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
 using BlazorGoogleCharts;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "F:\C_sharp\2021_12_27\EDS_calculator\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
+#line 9 "C:\Projects\Programs\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
 using BlazorGoogleCharts.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "F:\C_sharp\2021_12_27\EDS_calculator\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
+#line 10 "C:\Projects\Programs\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
 using System.IO;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "F:\C_sharp\2021_12_27\EDS_calculator\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
+#line 11 "C:\Projects\Programs\blazor-google-chartsV2\blazor-google-charts\_Imports.razor"
 using BlazorInputFile;
 
 #line default
